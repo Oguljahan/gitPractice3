e32a72c032cc250ecb7719d7bd443ce5c64bbd90
Oguljahan Atayeva <j.atayeva@outlook.com>
2020-04-24 14:07:06 -0500

    first commit

---
+++ src/day01/AllGood.java
@@ -0,0 +1,8 @@
+package day01;
+
+public class AllGood {
+    public static void main(String[] args) {
+        //I hope everything is Good.
+        System.out.println("Everything will be good");
+    }
+}
