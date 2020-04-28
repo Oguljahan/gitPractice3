c8cb833281d789373e42d4214a66b00cb54f7225
Oguljahan <64179705+Oguljahan@users.noreply.github.com>
2020-04-28 14:52:09 -0500

    Update HappyBirthday.java

--- src/day01/HappyBirthday.java
+++ src/day01/HappyBirthday.java
@@ -2,4 +2,7 @@ package day01;
 
 public class HappyBirthday {
     //Happy Birthday!!!
+    public static void main (String [] args){
+    System.out.println("Happy birthday B18 again, this line is edit from Github");
+}
 }
